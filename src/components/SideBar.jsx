5af43be985d0746b9f098a83b017b503df1d2b00
Lubna Fathima <lubnafathima275@gmail.com>
2024-11-23 20:49:26 +0530

    Feat: Updated Dashboard

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,22 @@
 import {
   CiHome,
+  CiGrid42,
   CiBellOn,
   CiBookmark,
   CiClock2,
   CiSettings,
   CiSearch,
 } from "react-icons/ci";
-import { MdOutlineVideoLibrary } from "react-icons/md";
 import { FaPlus } from "react-icons/fa6";
+import { FaUserFriends } from "react-icons/fa";
 
 const menuItems = {
   mainMenu: [
     { icon: <CiHome className="text-xl" />, label: "Home" },
-    { icon: <MdOutlineVideoLibrary className="text-xl" />, label: "My library" },
+    {
+      icon: <CiGrid42 className="text-xl" />,
+      label: "My library",
+    },
     { icon: <CiBellOn className="text-xl" />, label: "Notifications" },
     { icon: <CiBookmark className="text-xl" />, label: "Watch Later" },
     { icon: <CiClock2 className="text-xl" />, label: "History" },
@@ -20,14 +24,18 @@ const menuItems = {
   ],
   spaces: [
     { icon: <CiSearch className="text-xl" />, label: "Browse Spaces" },
-    { icon: <CiSearch className="text-xl" />, label: "My Space" }, 
+    { icon: null, label: "My Space" }, // This item will have a custom icon
   ],
 };
 
 const SideBar = () => {
   return (
     <div className="fixed w-72 h-screen bg-gray-50 md:flex flex-col gap-2 p-4 hidden">
-      
+      <h1 className="text-xl font-extrabold flex flex-wrap items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-full">
+        <FaUserFriends className="text-2xl" /> RBAC
+      </h1>
+      <hr className="border-t border-gray-300 my-4" />
+
       <div className="flex flex-col gap-4">
         {menuItems.mainMenu.map((item, index) => (
           <div key={index} className="flex items-center gap-2">
@@ -47,7 +55,13 @@ const SideBar = () => {
       <div className="flex flex-col gap-4 mt-4">
         {menuItems.spaces.map((item, index) => (
           <div key={index} className="flex items-center gap-2">
-            {item.icon}
+            {item.icon === null ? (
+              <div className="flex items-center justify-center w-8 h-8 rounded-full bg-blue-500 text-white font-semibold">
+                {item.label.charAt(0)}
+              </div>
+            ) : (
+              item.icon
+            )}
             <p>{item.label}</p>
           </div>
         ))}
